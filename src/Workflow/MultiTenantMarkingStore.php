c1361585377e64561250cd5c42392688dc7dcf42
Jolly Blume <jollyblume2@gmail.com>
2019-01-13 05:56:30 -0700

    added phpcr

---
+++ src/Workflow/MultiTenantMarkingStore.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Workflow;
+
+use Symfony\Component\Workflow\Marking;
+
+class MultiTenantMarkingStore {
+    public function getMarking(string $workflowUuid, string $subjectUuid) {
+
+    }
+
+    public function setMarking(string $workflowUuid, string $subjectUuid, Marking $marking) {
+
+    }
+}
