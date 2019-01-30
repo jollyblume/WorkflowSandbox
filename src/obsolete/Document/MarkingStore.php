faf77a14427cb8f9769aea229039dfab9f293b1d
Jolly Blume <jollyblume2@gmail.com>
2019-01-30 04:47:23 -0700

    old news

---
+++ src/obsolete/Document/MarkingStore.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace JBJ\Workflow\Document;
+
+use JBJ\Workflow\MarkingStoreInterface;
+use JBJ\Workflow\StoreCollectionInterface;
+use JBJ\Common\Collection\ArrayCollectionInterface;
+use JBJ\Common\Collection\GraphCollectionTrait;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+
+class MarkingStore implements MarkingStoreInterface, ArrayCollectionInterface
+{
+    use GraphCollectionTrait;
+
+    private $markingStoreId;
+
+    private $stores;
+
+    public function __construct(string $markingStoreId, array $elements = [], PropertyAccessorInterface $propertyAccessor = null)
+    {
+        $rules = [
+            'name' => [
+                'markingId',
+                'isDisabled' => false,
+                'isValid' => true,
+            ],
+            'parent' => [
+                'markingStore',
+                'isDisabled' => false,
+                'isValid' => true,
+            ],
+        ];
+        $this->initializeTrait($markingStoreId, $elements, $rules, $propertyAccessor);
+    }
+
+    public function getMarkingStoreId()
+    {
+        $markingStoreId = $this->name;
+        return $markingStoreId;
+    }
+
+    public function getStores()
+    {
+        return $this->stores;
+    }
+
+    public function setStores(?StoreCollectionInterface $stores)
+    {
+        $this->stores = $stores;
+    }
+}
